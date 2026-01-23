b9daf48f8ba40e4b416bdbfb1216f2ed96af4f5d
varomeas <valromeas43@gmail.com>
2026-01-23 18:36:16 +0100

    Changement de manette pour la fourchette, modification son télé, ajout des écritures

--- Assets/Scripts/Trier.cs
+++ Assets/Scripts/Trier.cs
@@ -1,17 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class Trier : MonoBehaviour
 {
     public string targetTag;
     public int score = 0;
+    public TextMeshProUGUI affichageScore;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger");
+        
         if (other.CompareTag(targetTag))
         {
             score++;
             Debug.Log($"Bien trié! Score {targetTag} : {score}");
+            affichageScore.text = score.ToString();
 
             //other.gameObject.GetComponent<Rigidbody>().isKinematic = false; si je veux empecher l'objet de bouger après
         }
