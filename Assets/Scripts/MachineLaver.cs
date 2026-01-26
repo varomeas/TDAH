15446c6ee0e8b6a1a524ab35ee1cbb1e8025debe
varomeas <valromeas43@gmail.com>
2026-01-26 20:40:21 +0100

    Vibration qui ne fonctionne plus

--- Assets/Scripts/MachineLaver.cs
+++ Assets/Scripts/MachineLaver.cs
@@ -12,6 +12,10 @@ public class MachineLaver : MonoBehaviour
     public AudioSource audio;
     public Animator machineAnimator;
 
+    [Header("Visuals des Mains")]
+    public Transform mainDroite;
+    public float tremblementMain = 0.05f;
+
     [Header("Paramètres audio")]
     public float volumeMax = 0.8f;
     public float dureeFadeIn = 5.0f;
@@ -21,6 +25,7 @@ public class MachineLaver : MonoBehaviour
     public HapticImpulsePlayer rightController;
 
     private bool isRunning = false;
+    private Vector3 posInitialeDroite;
 
     public void ActiverPerturbation(bool state)
     {
@@ -59,7 +64,7 @@ public class MachineLaver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (mainDroite) posInitialeDroite = mainDroite.localPosition;
     }
 
     // Update is called once per frame
@@ -69,17 +74,30 @@ public class MachineLaver : MonoBehaviour
         {
             Vector3 secousse = Random.insideUnitSphere * tremblement;
             assietteRB.AddForce(secousse, ForceMode.Acceleration);
-
+            Debug.Log("Je runnn");
             float intensiteActuelle = audio.volume/volumeMax;
             float vibrationForce = intensiteActuelle*vibrationMax;
             VibrerManette(leftController, vibrationForce);
             VibrerManette(rightController, vibrationForce);
+
+            TremblerMain(mainDroite, posInitialeDroite, intensiteActuelle);
+            
         }
         
     }
 
+    void TremblerMain(Transform mainTransform, Vector3 posInitiale, float ratio)
+    {
+        if (mainTransform != null)
+        {
+            Vector3 offset = Random.insideUnitSphere * (tremblementMain * ratio);
+            mainTransform.localPosition = posInitiale + offset;
+        }
+    }
+
     void VibrerManette(HapticImpulsePlayer controller, float amplitude)
     {
+        Debug.Log("je vibre");
         if (controller != null && amplitude > 0.01f)
         {
             controller.SendHapticImpulse(amplitude, Time.deltaTime);
